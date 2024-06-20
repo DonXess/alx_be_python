855cf0ad4d69ab237cd1dc6e753c9a55d98e7587
DonXess <loklari113@gmail.com>
2024-06-20 18:34:07 +0100

    add basic_operations script 2

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,5 @@
 number1 = 10
-number2= 5
+number2 = 5
 print ("the addition of",number1,"and",number2,"is",number1+number2)
 print ("the subtraction of",number1,"and",number2,"is",number1-number2)
 print ("the multiplication of",number1,"and",number2,"is",number1*number2)
